024ebf15530c73621999d67f3f9e3adc094575d5
Sean Huber <shuber@huberry.com>
2009-01-07 21:13:02 -0800

    Add comments/documentation to the active record related logic

--- lib/huberry/active_record.rb
+++ lib/huberry/active_record.rb
@@ -6,11 +6,30 @@ module Huberry
     
     protected
     
+      # Calls attr_encrypted with the options <tt>:encode</tt> and <tt>:marshal</tt> set to true
+      # unless they've already been specified
       def attr_encrypted(*attrs)
         options = { :encode => true, :marshal => true }.merge(attrs.last.is_a?(Hash) ? attrs.pop : {})
         super *(attrs << options)
       end
     
+      # Allows you to use dynamic methods like <tt>find_by_email</tt> or <tt>scoped_by_email</tt> for 
+      # encrypted attributes
+      #
+      # NOTE: This only works when the <tt>:key</tt> option is specified as a string (see the README)
+      #
+      # This is useful for encrypting fields like email addresses. Your user's email addresses 
+      # are encrypted in the database, but you can still look up a user by email for logging in
+      #
+      # Example
+      #
+      #   class User < ActiveRecord::Base
+      #     attr_encrypted :email, :key => 'secret key'
+      #   end
+      #
+      #   User.find_by_email_and_password('test@example.com', 'testing')
+      #   # results in a call to
+      #   User.find_by_encrypted_email_and_password('the_encrypted_version_of_test@example.com', 'testing')
       def method_missing_with_attr_encrypted(method, *args, &block)
         if match = /^(find|scoped)_(all_by|by)_([_a-zA-Z]\w*)$/.match(method.to_s)
           attribute_names = extract_attribute_names_from_match(match)
