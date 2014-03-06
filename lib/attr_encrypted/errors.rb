cab3fd0614d7bfa7ad33f7ad1a439329c43d5a9b
Tyler Pickett <t.pickett66@gmail.com>
2014-03-06 13:29:17 -0600

    Add a base error class for the project
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

---
+++ lib/attr_encrypted/errors.rb
@@ -0,0 +1,16 @@
+module AttrEncrypted
+  class Error < StandardError
+    unless instance_methods.include?(:cause)
+      attr_reader :cause
+
+      def initialize(msg = '', cause = $!)
+        super(msg)
+        @cause = cause
+      end
+    end
+  end
+
+  module Errors
+    class CipherError < Error; end
+  end
+end
