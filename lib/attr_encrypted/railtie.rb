5086d7bd09423a481c8816508d407504af6f5b86
Johnny Lai <johnny.lai@me.com>
2014-12-03 15:06:59 -0800

    CD-19412 Fix issue where attr_encrypted is undefined when rspec run * We need to load after protected_attributes, to avoid   https://github.com/attr-encrypted/attr_encrypted/issues/107 * We load after "active_record.set_configs" because protected_attributes loads before that

--- lib/attr_encrypted/railtie.rb
+++ lib/attr_encrypted/railtie.rb
@@ -2,7 +2,7 @@ require 'rails/railtie'
 
 module AttrEncrypted
   class Railtie < ::Rails::Railtie
-    config.after_initialize do
+    initializer "attr_encrypted.active_record", :after => "active_record.set_configs" do |app|
       Object.extend AttrEncrypted
 
       Dir[File.join(File.dirname(__FILE__), 'attr_encrypted', 'adapters', '*.rb')].each { |adapter| require adapter }
