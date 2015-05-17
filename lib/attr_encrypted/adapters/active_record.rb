372ff147855d581d2b384e231e1e20a7c1a06181
Douwe Maan <me@douwe.me>
2015-05-17 18:35:31 +0200

    Only lookup ActiveRecord column names when connected to the DB.

--- lib/attr_encrypted/adapters/active_record.rb
+++ lib/attr_encrypted/adapters/active_record.rb
@@ -56,7 +56,11 @@ if defined?(ActiveRecord::Base)
             # We add accessor methods of the db columns to the list of instance
             # methods returned to let ActiveRecord define the accessor methods
             # for the db columns
-            if table_exists?
+            
+            # Use with_connection so the connection doesn't stay pinned to the thread.
+ 	          connected = ::ActiveRecord::Base.connection_pool.with_connection(&:active?) rescue false
+
+            if connected && table_exists?
               columns_hash.keys.inject(super) {|instance_methods, column_name| instance_methods.concat [column_name.to_sym, :"#{column_name}="]}
             else
               super
