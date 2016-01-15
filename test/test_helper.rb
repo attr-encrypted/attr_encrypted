cf9ad225c77f135804624ae45891fece02f625ac
Stephen Aghaulor <saghaulor@gmail.com>
2016-01-15 14:11:50 -0800

    Updated SimpleCov::Formatter::MultiFormatter specification.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,11 +2,13 @@ require 'simplecov'
 require 'simplecov-rcov'
 require "codeclimate-test-reporter"
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  SimpleCov::Formatter::RcovFormatter,
-  CodeClimate::TestReporter::Formatter
-]
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  [
+    SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::RcovFormatter,
+    CodeClimate::TestReporter::Formatter
+  ]
+)
 
 SimpleCov.start do
   add_filter 'test'
